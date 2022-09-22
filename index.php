6e423bc2a9bafbf7d17ec38bdb508a4947001fab
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-23 00:02:26 +0300

    🦴 The HTML skeleton has been implemented.

--- index.php
+++ index.php
@@ -1,21 +1,35 @@
 <?php
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
-?>
-<div id="fingerprint"></div>
-<script>
-  // Initialize the agent at application startup.
-  const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
-    .then(FingerprintJS => FingerprintJS.load())
 
-  // Get the visitor identifier when you need it.
-  fpPromise
-    .then(fp => fp.get())
-    .then(result => {
-      // This is the visitor identifier:
-      const visitorId = result.visitorId
-      document.querySelector('#fingerprint').innerHTML = visitorId;
-    })
-    .catch(error => console.error(error))
-</script>
+$domain = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER["HTTP_HOST"];
+$url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER["HTTP_HOST"] . $_SERVER['REQUEST_URI'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="tr-TR">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Language" content="tr" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="msapplication-tap-highlight" content="no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="robots" content="index, follow" />
+    <meta name="googlebot" content="index, follow" />
+    <meta name="revisit-after" content="7 Days" />
+    <title>Muş Alparslan Üniversitesi Bilişim Sistemleri ve Teknolojileri Yoklama Sistemi</title>
+    <meta name="description" content="Muş, Alparslan Üniversitesi, Bilişim Sistemleri ve Teknolojileri" />
+    <meta name="keywords" content="Muş, Alparslan Üniversitesi, Bilişim Sistemleri ve Teknolojileri" />
+    <meta name="theme-color" content="#1A73E8" />
+    <meta name="msapplication-TileColor" content="#1A73E8" />
+    <!-- <meta name="msapplication-TileImage" content="localhost/icon.png" /> -->
+    <link rel="canonical" href="<?= $url ?>" />
+    <!-- <link rel="icon" type="png/image" href="localhost/icon.png" sizes="32x32" /> -->
+    <!-- <link rel="stylesheet" href="localhost/style.css"> -->
+</head>
+<body>
+
+    içerik
+
+</body>
+</html>
