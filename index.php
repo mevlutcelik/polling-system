95d5b81cc121c0a2313182f97c54f896f34b784c
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-09-23 15:53:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,27 +7,68 @@ $url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER["HTTP_HOST"] . $_SERVER['RE
 
 <!DOCTYPE html>
 <html lang="tr-TR">
+
+
+
 <head>
+
+
     <meta charset="UTF-8" />
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta http-equiv="Content-Language" content="tr" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+
+
     <meta name="msapplication-tap-highlight" content="no" />
+
+
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+
+
     <meta name="robots" content="index, follow" />
     <meta name="googlebot" content="index, follow" />
+    
+    
+    
     <meta name="revisit-after" content="7 Days" />
+
+
+
     <title>Muş Alparslan Üniversitesi Bilişim Sistemleri ve Teknolojileri Yoklama Sistemi</title>
     <meta name="description" content="Muş, Alparslan Üniversitesi, Bilişim Sistemleri ve Teknolojileri" />
     <meta name="keywords" content="Muş, Alparslan Üniversitesi, Bilişim Sistemleri ve Teknolojileri" />
+
+
+
     <meta name="theme-color" content="#1A73E8" />
     <meta name="msapplication-TileColor" content="#1A73E8" />
-    <!-- <meta name="msapplication-TileImage" content="localhost/icon.png" /> -->
+
+
+
     <link rel="canonical" href="<?= $url ?>" />
+
+
+
+    <!-- <meta name="msapplication-TileImage" content="localhost/icon.png" /> -->
     <!-- <link rel="icon" type="png/image" href="localhost/icon.png" sizes="32x32" /> -->
-    <!-- <link rel="stylesheet" href="localhost/style.css"> -->
+    
+    
+    
+    <link rel="stylesheet" href="<?= $domain ?>/css/style.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap" rel="stylesheet">
+
+
+
 </head>
+
+
+
 <body>
+
+
 
     <div id="app">
 
@@ -50,6 +91,8 @@ $url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER["HTTP_HOST"] . $_SERVER['RE
 
 
     </div>
+
+
 
 </body>
 </html>
